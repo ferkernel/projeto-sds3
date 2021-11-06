9ce0ee1c44d6972ebbb02c1ca46be948250f158f
Marcos Fernandes <ferkernel@outlook.com>
2021-11-06 18:56:52 -0300

    Basic static  components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">OLÁ MUNDO</h1>
+      <DataTable />
     </div>
+
+    <Footer />
+   
+    </>
   );
 }
 
